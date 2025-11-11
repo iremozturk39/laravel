9e9088d714397fda3df943cbcaf7885b1e3c76f8
İrem Öztürk <iremozturk39@outlook.com>
2025-11-11 11:55:45 +0300

    temiz laravel kurulumu

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TempController;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/hello', function() {
-    return 'Hello, Laravel!';
-});
-
-Route::get('/hello/{name}', function($name) {
-    return 'Hello, ' . $name . ' !!!';
-
-});
-
-Route::get('/tmp', [TempController::class, 'tmpFunction']);
